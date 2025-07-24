20617925a3ea45f57fe1dc326b91eab99e08e6b5
alissoncavalcanti <alissoncavalcanti1@yahoo.com.br>
2025-07-23 21:13:29 -0300

    Novos testes de importação e exportação

---
+++ importacao/main.js
@@ -0,0 +1,36 @@
+
+const { gets, print } = require('./funcoes-auxiliares');
+
+/*
+Uma sala contém 5 alunos e para cada aluno foi sorteado um número de 1 a 100.
+Faça um programa que receba os 5 números sorteados para os alunos e mostre o maior número sorteado.
+
+Dados de entrada:
+5
+50
+10
+98
+23
+
+Saíde:
+98
+*/
+
+const numerosSorteados = [];
+
+for (let i = 0; i < 5; i++) {
+    const numeroSorteado = gets();
+    numerosSorteados.push(numeroSorteado);    
+}
+
+let maiorValor = 0;
+
+
+for (let i = 0; i < numerosSorteados.length; i++) {
+    const numeroSorteado = numerosSorteados[i];
+    if ( numeroSorteado > maiorValor ) {
+        maiorValor = numeroSorteado;
+    }
+}
+
+print(maiorValor);
